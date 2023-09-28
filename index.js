16845bda47dd1d6af5d120b1e89b65741f0749a7
Mithilesh <mithilesh.shah@iratechnologies.com>
2023-09-28 18:12:29 +0530

    code refactored

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
 const express = require("express");
-const ToDo = require("./models/to-do");
 require("./config");
 
 const toDoRoutes = require('./routers/to-do');
 
 const app = express();
 
-// const api = "/api/v1/to-do";
-
 app.use(express.json());
 
 app.use('/api/v1/to-do',toDoRoutes);
 
-// Create to-do
-// app.post(`${api}/create`, async (req, res) => {
-//   const data = new ToDo(req.body);
-//   const savedToDo = await data.save();
-//   res.send(savedToDo);
-// });
-
-// // Get all todo's
-// app.get(`${api}/all`, async (req, res) => {
-//   const toDoList = await ToDo.find();
-//   res.send(toDoList);
-// });
-
-// // Get to-do by id
-// app.get(`${api}/:_id`, async (req, res) => {
-//   const toDo = await ToDo.findOne(req.params);
-//   res.send(toDo);
-// });
-
-// // Update to-do
-// app.put(`${api}/:_id`, async (req, res) => {
-//   await ToDo.updateOne(req.params, { $set: req.body });
-
-//   const updateRes = {
-//     message: "To-do updated successfully",
-//     toDoId: req.params._id,
-//   };
-
-//   res.send(updateRes);
-// });
-
-// // Delete to-do by id
-// app.delete(`${api}/:_id`, async (req, res) => {
-//   await ToDo.deleteOne(req.params);
-//   const deleteRes = {
-//     message: "To-do deleted successfully",
-//     toDoId: req.params._id,
-//   };
-//   res.send(deleteRes);
-// });
-
-
-// app.get(`${api}/search/:key`,async(req,res)=>{
-//     const searchedData= await ToDo.find({
-//          "$or":[
-//             {"name":{$regex: req.params.key}},
-//             {"status":{$regex: req.params.key}},
-//          ]
-//     });
-
-//     res.send(searchedData);
-
-// });
 
 app.listen(3000);
