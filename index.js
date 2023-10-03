51f285688455e345cfb70d92d51205daabdeb6dc
Mithilesh <mithilesh.shah@iratechnologies.com>
2023-10-03 10:14:13 +0530

    Cors issue resolved.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.options('*', cors());
 require("./config");
 
 const toDoRoutes = require('./routers/to-do');
 
-const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/to-do',toDoRoutes);
-
 
 app.listen(3000);
